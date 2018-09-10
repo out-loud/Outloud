611c7911e62bf1a72727a9d65503f44373ff1ee9
Marcin Miazga <miazgamarcin@outlook.com>
2018-09-10 22:33:10 +0200

    update start.sh script

--- continuous-integration/start.sh
+++ continuous-integration/start.sh
@@ -6,24 +6,21 @@ REGISTRY=$3
 BRANCH=$4
 
 echo "### Pulling"
-.git-pull.sh
+./git-pull.sh
 echo -e "\n"
 
 echo "### Building"
-.\docker-build.sh $ENVIRONMENT
+./docker-build.sh $ENVIRONMENT
 echo -e "\n"
 
 echo "### Testing"
-.\docker-test.sh $IMAGE_NAME $ENVIRONMENT
+./docker-test.sh $IMAGE_NAME $ENVIRONMENT
 echo -e "\n"
 
 echo "### Cleaning"
-.\docker-clean.sh $IMAGE_NAME $ENVIRONMENT
+./docker-clean.sh $IMAGE_NAME $ENVIRONMENT
 echo -e "\n"
 
 echo "### Publishing"
-.\docker-publish.sh $IMAGE_NAME $REGISTRY $BRANCH
+./docker-publish.sh $IMAGE_NAME $REGISTRY $BRANCH
 echo -e "\n"
-
-
-read -p "Press enter to continue"
