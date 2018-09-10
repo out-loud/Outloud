bf14e0312f98cee16848b586e8c7fe8bdffbbc0f
Marcin Miazga <miazgamarcin@outlook.com>
2018-09-10 21:39:41 +0200

    shorten git pull

--- git-pull.sh
+++ git-pull.sh
@@ -3,9 +3,6 @@ cd ..
 REPOSITORIES=(Outloud.QuizService)
 for REPOSITORY in ${REPOSITORIES[*]}
 do
-	 echo ========================================================
-	 echo Pulling repository: $REPOSITORY
-	 echo ========================================================
      git clone https://github.com/out-loud/$REPOSITORY
 done
 read -p "Press enter to continue"
