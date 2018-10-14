3a09aa58a21c385a0b6badd7d49e191454e3477c
Marcin Miazga <miazgamarcin@outlook.com>
2018-10-14 17:12:36 +0200

    update docker-clean script

--- continuous-integration/docker-clean.sh
+++ continuous-integration/docker-clean.sh
@@ -6,7 +6,7 @@ dockerComposeOverridePath=`readlink -f "./docker-compose.${environment}.yml"`
 echo -e "\n"
 docker-compose -f $dockerComposePath -f $dockerComposeOverridePath down
 echo -e "\n"
-docker rmi $(docker images --quiet $testImageName)
+docker rmi $(docker images --quiet)
 echo -e "\n"
 docker image prune --force
 echo -e "\n"
