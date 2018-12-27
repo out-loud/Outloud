1ebd170384c073cdeaa3a94650940a1c314ae6bb
Marcin Miazga <miazgamarcin@outlook.com>
2018-12-27 10:56:15 +0100

    updated docker-build script with image name for compose

--- continuous-integration/docker-build.sh
+++ continuous-integration/docker-build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ENVIRONMENT=$1
-NUGET_VERSION=$2
-NUGET_API_KEY=$3
+IMAGE_NAME=$1
+ENVIRONMENT=$2
+NUGET_VERSION=$3
+NUGET_API_KEY=$4
 
 export NUGET_VERSION
 export NUGET_API_KEY
 
 DOCKER_COMPOSE_PATH=`readlink -f "../Outloud/docker-compose.${ENVIRONMENT}.yml"`
 
-docker-compose -f $DOCKER_COMPOSE_PATH build
+docker-compose -f $DOCKER_COMPOSE_PATH build $IMAGE_NAME
